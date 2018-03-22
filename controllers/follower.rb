6abc0b6572e301ffd32ad1c80efffb5ed424aa6b
Lizzie Koshelev <lizkosheleva@gmail.com>
2018-03-22 16:02:20 -0400

    add followers

--- controllers/follower.rb
+++ controllers/follower.rb
@@ -1,19 +1,18 @@
-get_follower_count
 
-  def get_follower_count
+  def get_follower_count(user)
   end
 
-  def get_following_count
+  def get_following_count(user)
   end
 
-  def incr_following_count
+  def incr_following_count(user)
   end
 
-  def decr_following_count
+  def decr_following_count(user)
   end
 
-def  incr_following_count
+def  incr_following_count(user)
 end
 
-def decr_following_count
+def decr_following_count(user)
 end
