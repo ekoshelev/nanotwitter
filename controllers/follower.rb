0d08e43e2d1714e35cc0509874b4c1ed731fbb91
Lizzie Koshelev <lizkosheleva@gmail.com>
2018-03-22 16:01:36 -0400

    add following methodds'

---
+++ controllers/follower.rb
@@ -0,0 +1,19 @@
+get_follower_count
+
+  def get_follower_count
+  end
+
+  def get_following_count
+  end
+
+  def incr_following_count
+  end
+
+  def decr_following_count
+  end
+
+def  incr_following_count
+end
+
+def decr_following_count
+end
