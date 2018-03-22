05cb7a2555f7c1f7f61ca40b9adeba6faebae43a
Lizzie Koshelev <lizkosheleva@gmail.com>
2018-03-22 16:16:11 -0400

    redis pseudocode

---
+++ controllers/follower_redis.rb
@@ -0,0 +1,46 @@
+
+  def get_follower_count(user)
+
+    if it doesnt exist
+    Redis::put(make_follower_id(user),1)
+
+    return 0
+  else {
+    return  Redis::get(make_follower_id(user))
+  }
+
+  end
+
+  def get_following_count(user)
+    if it doesnt exist
+    Redis::put(make_following_id(user),1)
+
+    return 0
+  else {
+    return  Redis::get(make_following_id(user))
+  }
+  end
+
+  def incr_following_count(user)
+      Redis::increment(make_following_id(user))
+  end
+
+  def decr_following_count(user)
+        Redis::decrement(make_following_id(user))
+  end
+
+def  incr_follower_count(user)
+    Redis::increment(make_follower_id(user))
+end
+
+def decr_follower_count(user)
+      Redis::decrement(make_follower_id(user))
+end
+
+
+def make_following_id(user)
+  return user + "_following";
+end
+def make_follower_id(user)
+      return user + "_followers";
+end
