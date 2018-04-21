cc1d63b66bef9258d92fcdc447e934511decdcce
sparkit2002 <rielyallen@gmail.com>
2018-04-21 17:24:23 -0400

    fix...?

--- routs/test_interface_routs.rb
+++ routs/test_interface_routs.rb
@@ -9,7 +9,6 @@ require 'json'
 require './controllers/return_timeline.rb'
 require './controllers/twitter_functionality.rb'
 require_relative '../temp/fry_seeding.rb'
-Dir["./types/*.rb"].each {|file| require file}
 Dir["./models/*.rb"].each {|file| require file}
 
 
