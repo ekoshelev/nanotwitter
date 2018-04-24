fa3110b4b3d649afb56f6849934e5a73eeae693b
sparkit2002 <rielyallen@gmail.com>
2018-04-24 16:04:23 -0400

    added test loader

--- app.rb
+++ app.rb
@@ -266,9 +266,15 @@ class TestApp < Sinatra::Base
   register Sinatra::Twitter::Bootstrap::Assets
 end
 
-get '/loaderio-b824862f1b513a533572fb2d3c56d0b3/' do
-	 'loaderio-b824862f1b513a533572fb2d3c56d0b3'
+#new loader
+get '/loaderio-f58d25c9cfc33c94fb8c771be91e2848/' do
+    'loaderio-f58d25c9cfc33c94fb8c771be91e2848'
 end
+
+#old loader
+# get '/loaderio-b824862f1b513a533572fb2d3c56d0b3/' do
+# 	 'loaderio-b824862f1b513a533572fb2d3c56d0b3'
+# end
 
 
 
