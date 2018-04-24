6395da8bfd884fa47ba8f52e8650b6bc15089cd9
Lizzie Koshelev <lizkosheleva@gmail.com>
2018-04-24 16:36:17 -0400

    got rid of redis

--- app.rb
+++ app.rb
@@ -8,10 +8,8 @@ require 'uri'
 require 'newrelic_rpm'
 require 'graphql'
 require 'json'
-require 'redis'
 require 'bcrypt'
 #require 'byebug'
-require './controllers/follower_controller.rb'
 require './controllers/return_timeline.rb'
 require './controllers/twitter_functionality.rb'
 require_relative './temp/fry_seeding.rb'
@@ -23,6 +21,7 @@ Dir["./models/*.rb"].each {|file| require file}
 
 require_relative 'temp/fry_test_001.rb'
 require_relative './temp/fry_seeding.rb'
+
 configure do
   enable :sessions
 end
@@ -34,38 +33,15 @@ helpers do
   end
 end
 
+
+
 before do
-  @redis = Redis.new(url: ENV["REDIS_URL"], connect_timeout: 0.2,
-  read_timeout: 1.0,
-  write_timeout: 0.5
-)
-  @redis.quit
-  @tweets= Tweet.all
-  @followers=Follower.all
-  @users = User.all
-  @followercontroller=FollowerController.new(@redis, @users)
-	@timeclass=ReturnTimeline.new(@redis, @tweets, @followers,@followercontroller)
+	@timeclass=ReturnTimeline.new
 	@twitter_functionality = TwitterFunctionality.new
-
-  @testuser = User.find_by(name: "TestUser")
-  if @testuser == nil
-    @testuser = @twitter_functionality.create_test_user
-  end
-
-
 end
 
 get '/' do
-  #@hometweets= @timeclass.return_recent_tweets
-  #@timeclass.quitRedis
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
-    @hometweets=   @timeclass.get_main_timeline
-    @timeclass.quitRedis
-  else
-    timeline = @timeclass.return_recent_tweets
-    @hometweets= @twitter_functionality.timeline_to_hash(timeline)
-  end
+	@hometweets= @timeclass.return_recent_tweets
 	erb :index
 end
 
@@ -95,11 +71,7 @@ post '/retweet' do
 	@retweet = params[:retweet]
 	@result = Tweet.new(@retweet)
 	@result.save
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
 	@tweets = @timeclass.return_timeline_by_user( session[:user])
-  @timeclass.quitRedis
-end
 	@followers = Follower.all
 	erb :display
 end
@@ -109,15 +81,9 @@ post '/followprofile' do
 	@result = Follower.new(@follow)
 	@result.save
   @user = User.find_by_id( @follow[:user_id])
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
-  @followercontroller.incr_following(session[:user].id,@follow[:user_id])
-	@usertweets = @timeclass.get_user_tweets(@user)#@timeclass.return_tweets_by_user( @follow[:user_id])
-	@followers = @followercontroller.get_followers( @follow[:user_id])
-	@following = @followercontroller.get_following(  @follow[:user_id])
-  @timeclass.add_user_timeline(@usertweets,session[:user])
-  @timeclass.quitRedis
-end
+	@usertweets = @timeclass.return_tweets_by_user( @follow[:user_id])
+	@followers = @timeclass.return_follower_list( @follow[:user_id])
+	@following = @timeclass.return_following_list( @follow[:user_id])
 	erb :profile
 end
 
@@ -125,72 +91,24 @@ post '/unfollowprofile' do
 	@unfollow = params[:unfollow]
 	follower =  Follower.find_by(follower: @unfollow[:follower_id], user_id: @unfollow[:user_id])
 	follower.delete
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
-  @followercontroller.decr_following(@unfollow[:follower_id],@unfollow[:user_id])
   @user = User.find_by_id( @unfollow[:user_id])
-	@usertweets = @timeclass.get_user_tweets(@user)#@timeclass.return_tweets_by_user( @unfollow[:user_id])
-	@followers = @followercontroller.get_followers( @unfollow[:user_id])
-	@following = @followercontroller.get_following( @unfollow[:user_id])
-  @timeclass.remove_user_timeline(@user,session[:user])
-  @timeclass.quitRedis
-end
+	@usertweets = @timeclass.return_tweets_by_user( @unfollow[:user_id])
+	@followers = @timeclass.return_follower_list( @unfollow[:user_id])
+	@following = @timeclass.return_following_list( @unfollow[:user_id])
 	erb :profile
 end
 
 get '/display' do
-	#@tweets = @timeclass.return_timeline_by_user( session[:user])
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
-    @tweets = @timeclass.get_user_timeline(session[:user])
-    @timeclass.quitRedis
-  else
-    timeline = @timeclass.return_timeline_by_user(session[:user])
-    @tweets = @twitter_functionality.timeline_to_hash(timeline)["tweets"]
-  end
-
+	@tweets = @timeclass.return_timeline_by_user( session[:user])
 	erb :display
 end
 
-post '/api/token/new' do
-  @token=Faker::Crypto.unique.md5
-		session[:user].api_token = @token
-		session[:user].save
-  @user=session[:user]
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
-  @usertweets = @timeclass.return_tweets_by_user( session[:user].id)
-  @followers = @followercontroller.get_followers( session[:user].id)
-  @following = @followercontroller.get_following( session[:user].id)
-  @timeclass.quitRedis
-end
-	erb :profile
-end
 
 get '/profile/:id' do
-
   @user = User.find_by_id(params[:id])
-  @timeclass.connectRedis
-  if (@redis._client.connected?)
-    @followercontroller
-    @usertweets = @timeclass.get_user_tweets(@user)#@timeclass.return_tweets_by_user(params[:id])
-    byebug
-    @followers =@followercontroller.get_followers(params[:id])
-  	@following = @followercontroller.get_following(params[:id])
-    @timeclass.quitRedis
-  else
-    timeline = @timeclass.return_tweets_by_user(params[:id])
-    @usertweets = @twitter_functionality.timeline_to_hash(timeline)
-    followers = @timeclass.return_follower_list(@user.id)
-    following = @timeclass.return_following_list(@user.id)
-    @followers = @twitter_functionality.follower_to_int(followers)
-    @following = @twitter_functionality.following_to_int(following)
-
-  end
-  @token = ""
-  if session[:user] != nil && session[:user].id==(params[:id]).to_i
-		@token = session[:user].api_token
-  end
+  @usertweets = @timeclass.return_tweets_by_user(params[:id])
+	@followers = @timeclass.return_follower_list(params[:id])
+	@following = @timeclass.return_following_list(params[:id])
 	erb :profile
 end
 
@@ -207,11 +125,7 @@ post '/login' do
 end
 
 get '/login_fail' do
-    @timeclass.connectRedis
-  if (@redis._client.connected?)
   @hometweets= @timeclass.return_recent_tweets
-  @timeclass.quitRedis
-end
   erb :login_fail
 end
 
@@ -226,21 +140,8 @@ post '/post_tweet' do
 	@result.save
   @twitter_functionality.add_hashtags(@result)
   @twitter_functionality.add_mentions(@result)
-  @result.text = @twitter_functionality.display_tweet(@result)
-  @timeclass.connectRedis
-if (@redis._client.connected?)
-  @timeclass.post_tweet_home_timeline(@result)
-  @timeclass.post_tweet_redis(@result)
-  @timeclass.quitRedis
-end
-
+	@tweets = Tweet.all
 	redirect '/display'
-end
-
-get '/hashtags/:id' do
-  @hashtag = Hashtag.find_by(id: params[:id])
-  @hashtagtweets = @hashtag.tweets.sort_by{ |k| k["time_created"] }.reverse!
-  erb :hashtags
 end
 
 post '/register' do
@@ -266,12 +167,10 @@ class TestApp < Sinatra::Base
   register Sinatra::Twitter::Bootstrap::Assets
 end
 
-#new loader
-get '/loaderio-f58d25c9cfc33c94fb8c771be91e2848/' do
-    'loaderio-f58d25c9cfc33c94fb8c771be91e2848'
-end
+#Test Interface HTTP calls
 
-#old loader
+
+
 get '/loaderio-b824862f1b513a533572fb2d3c56d0b3/' do
 	 'loaderio-b824862f1b513a533572fb2d3c56d0b3'
 end
@@ -279,12 +178,22 @@ end
 
 
 #Root of GraphQL Based API
-post '/api/v1/graphql' do
 
-		request_payload = JSON.parse(request.body.read)
 
-	  result = NanoTwitterAPI.execute(request_payload['query'])
+post '/api/token/new' do
+	if session[:user] != nil
+		session[:user].api_token = Faker::Crypto.unique.md5
+		session[:user].save
+	end
+	redirect to('/api/token/view')
+end
 
-	  result.to_json
+get '/api/token/view' do
+	if session[:user] != nil
+		@token = session[:user].api_token
+	else
+		@token = "You must sign in to view your API token"
+	end
 
+	erb :api_token
 end
--- routs/test_interface_routs.rb
+++ routs/test_interface_routs.rb
@@ -10,7 +10,6 @@ require './controllers/return_timeline.rb'
 require './controllers/twitter_functionality.rb'
 require 'csv'
 require 'activerecord-import'
-require './db/seedredis.rb'
 require_relative '../temp/fry_seeding.rb'
 Dir["./models/*.rb"].each {|file| require file}
 
@@ -125,9 +124,6 @@ post '/test/reset/standard' do
   # seed_table("users.csv", "users", "(name, email, password, api_token)", params[:users])
   # seed_table("tweets.csv", "tweets", "(text, time_created, user_id)", params[:tweets])
   # seed_table("follows.csv", "followers", "(user_id, follower_id)", params[:follows])
-  @seed_redis = SeedRedis.new
-  @seed_redis.put_tweets_into_redis
-  @seed_redis.put_followers_into_redis
 	return 200
 end
 
